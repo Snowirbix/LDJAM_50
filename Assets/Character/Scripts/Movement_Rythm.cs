d77dd6ca9e6ec2179906cc456e62e400c565db18
LeTalSkyppy <alexandre.monnier@viacesi.fr>
2022-04-02 20:32:21 +0200

    movement fix, test trigger ok, rotation on direction.

--- Assets/Character/Scripts/Movement_Rythm.cs
+++ Assets/Character/Scripts/Movement_Rythm.cs
@@ -10,14 +10,21 @@ public class Movement_Rythm : MonoBehaviour
     private CharacterController characterController;
     private Character_Controls inputActions;
     private Vector2 movement;
-    public float jumpHeight = 5f;
-    public float jump = 5f;
-    public float gravity = -9.8f;
+    public float jumpHeight = 0.5f;
 
-    private float y;
+    [Range(0, 100)]
+    public float aerialControlPercentage = 50;
+
+    private float gravity = Physics.gravity.y;
+
+    private Vector3 velocity;
+
+    public Transform characterTransform;
+
+    private Vector3 forward;
 
     [Range(1,10)]
-    public float speed = 5f;
+    public float speed = 4f;
 
     public float delayJump = 0.1f;
 
@@ -26,33 +33,61 @@ public class Movement_Rythm : MonoBehaviour
         rigidbody = GetComponent<Rigidbody>();
         playerInput = GetComponent<PlayerInput>();
         characterController = GetComponent<CharacterController>();
+        
 
         inputActions = new Character_Controls();
         inputActions.Enable();
         inputActions.Player.Jump.performed += _ => Jump();
-        y = gravity;
+
+        forward = Vector3.forward;
     }
 
     public void Jump()
     {
-        Debug.Log("Jump");
         if (characterController.isGrounded)
         {
-            y = jump;
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
     }
 
     private void Update()
     {
-        if(transform.position.y > jumpHeight)
-        {
-            y = gravity;
-        }
         movement = inputActions.Player.Movement.ReadValue<Vector2>();
-        characterController.Move(new Vector3(movement.x, y, movement.y) * speed * Time.deltaTime);
-        if (characterController.isGrounded)
-        {
+        Vector3 move = transform.right * movement.x;
 
+        
+
+        if(movement.x > 0 && forward.z < 0)
+        {
+            forward = Vector3.forward;
         }
+
+        if(movement.x < 0 && forward.z > 0)
+        {
+            forward = -forward;
+        }
+
+        if (characterController.isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+            characterController.Move(move * speed * Time.deltaTime);
+        }
+        else
+        {
+            characterController.Move(move * speed * (aerialControlPercentage/100) * Time.deltaTime);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        characterController.Move(velocity * Time.deltaTime);
+        //characterTransform.rotation = characterTransform.rotation * Quaternion.AngleAxis(-1f,Vector3.up);
+
+        characterTransform.rotation = Quaternion.Lerp(characterTransform.rotation, Quaternion.LookRotation(forward), 0.05f);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other);
     }
 }
