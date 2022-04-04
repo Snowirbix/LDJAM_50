1744c92305d833e1f813da68f43ad4af381ee5d3
Snowirbix <snowirbix@gmail.com>
2022-04-04 19:02:00 +0200

    Attack animation

--- Assets/Character/Scripts/DamageReceive.cs
+++ Assets/Character/Scripts/DamageReceive.cs
@@ -1,33 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DamageReceive : MonoBehaviour
 {
     public Health health;
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(other.gameObject.name);
-        if (other.tag.Contains("Player"))
+        if (other.name.Contains("SWORD"))
         {
-            Debug.Log("touch by the player");
             health.Damage(10);
         }
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log(collision.gameObject.name);
     }
 }
