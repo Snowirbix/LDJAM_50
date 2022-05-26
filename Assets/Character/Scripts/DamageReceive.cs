83ebd6d6b7dec63db68ff768bd37c3bfdfaad4f3
LeTalSkyppy <alexandre.monnier@viacesi.fr>
2022-05-26 22:54:45 +0200

    [Refacto] Clean up DamageReceive + new Method in Health script

--- Assets/Character/Scripts/DamageReceive.cs
+++ Assets/Character/Scripts/DamageReceive.cs
@@ -9,21 +9,43 @@ public class DamageReceive : MonoBehaviour
     public float invicibility = 0.5f;
     private float lastHit = 0f;
 
+    private static string SWORD_OBJECT_NAME = "SWORD";
+    private int damageOnHit = 10;
+    private Vector3 effectOffsetPosition = new Vector3(0, 0, 0.2f);
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.name.Contains("SWORD"))
+        if (IsSwordCollider(other) && IsVulnerable() && health.IsAlive())
         {
-            if (Time.time > lastHit + invicibility)
-            {
-                if (!health.isDead)
-                {
-                    var hit = other.gameObject.GetComponent<Collider>().ClosestPointOnBounds(transform.position);
-                    hitEffect.transform.position = hit + new Vector3(0, 0, 0.2f);
-                    hitEffect.Play();
-                    lastHit = Time.time;
-                    health.Damage(10);
-                }
-            }
+            PlayHitEffect(other.ClosestPointOnBounds(transform.position));
+            ResetLastHitTime();
+            ApplyDamageOnHealth(damageOnHit);
         }
+    }
+
+    private bool IsSwordCollider(Collider collider)
+    {
+        return collider.name.Contains(SWORD_OBJECT_NAME);
+    }
+
+    private bool IsVulnerable()
+    {
+        return Time.time > lastHit + invicibility;
+    }
+
+    private void PlayHitEffect(Vector3 hitPosition)
+    {
+        hitEffect.transform.position = hitPosition + effectOffsetPosition;
+        hitEffect.Play();
+    }
+
+    private void ResetLastHitTime()
+    {
+        lastHit = Time.time;
+    }
+
+    private void ApplyDamageOnHealth(int damageOnHit)
+    {
+        health.Damage(damageOnHit);
     }
 }
--- Assets/Character/Scripts/Health.cs
+++ Assets/Character/Scripts/Health.cs
@@ -41,4 +41,9 @@ public class Health : MonoBehaviour
         }
     }
 
+    public bool IsAlive()
+    {
+        return !isDead;
+    }
+
 }
